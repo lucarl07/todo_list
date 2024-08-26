716b26a16525f1564fa7843039c9edcd0331c2c0
lucarl07 <junitinho2007@gmail.com>
2024-08-26 08:15:40 -0300

    🚧 Build the basic server requisites

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log("Hello world!")
+// Dependências
+import "dotenv/config"
+import express from "express"
+import cors from "cors";
+
+// Porta do Servidor
+const PORT = process.env.PORT || 3333
+
+// Inicializando o Express
+const app = express();
+
+// Middlewares Principais
+app.use(cors())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())

c1151ccfc3245f2a7be78776db92e8e96f61c1fd
lucarl07 <junitinho2007@gmail.com>
2024-08-28 10:08:08 -0300

    🐛 fix: Middleware validation not working

--- src/middleware/checkNewTaskBody.js
+++ src/middleware/checkNewTaskBody.js
@@ -2,12 +2,12 @@ const validation = (req, res, next) => {
   const { nome, descricao } = req.body
 
   if (!nome) {
-    res.status(400).json({
+    return res.status(400).json({
       err: "O nome da tarefa é obrigatório."
     })
   }
   if (!descricao) {
-    res.status(400).json({
+    return res.status(400).json({
       err: "A descrição da tarefa é obrigatória."
     })
   }
--- src/middleware/checkTaskUpdate.js
+++ src/middleware/checkTaskUpdate.js
@@ -1,27 +1,25 @@
 const validation = (req, res, next) => {
   const { id } = req.params
-
-  if (!id) {
-    res.status(400).json({
-      err: "A solicitação exige que seja passado um ID."
-    })
-  }
-
   const { nome, descricao, status } = req.body
 
+  if (!id) {
+    return res.status(400).json({
+      message: "A solicitação exige que seja passado um ID."
+    })
+  }
   if (!nome) {
-    res.status(400).json({
-      err: "O nome da tarefa é obrigatório."
+    return res.status(400).json({
+      message: "O nome da tarefa é obrigatório."
     })
   }
   if (!descricao) {
-    res.status(400).json({
-      err: "A descrição da tarefa é obrigatória."
+    return res.status(400).json({
+      message: "A descrição da tarefa é obrigatória."
     })
   }
   if (!status) {
-    res.status(400).json({
-      err: "O status da tarefa é obrigatório."
+    return res.status(400).json({
+      message: "O status da tarefa é obrigatório."
     })
   }
 

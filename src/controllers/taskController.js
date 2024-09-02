bfb041dd94f8dead065ebe47c6ecfacf91bc8e73
lucarl07 <junitinho2007@gmail.com>
2024-09-02 11:14:34 -0300

    ☑️ validators: Modulize updateSchema

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -3,6 +3,8 @@ import formatZodError from "../helpers/formatZodError.js";
 import Tarefa from "../models/taskModel.js";
 
 // Validações com Zod:
+import updateSchema from "../validators/updateSchema.js";
+
 const createSchema = z.object({
   nome: 
     z.string()
@@ -19,24 +21,6 @@ const taskIdSchema = z.object({
   id: z.string().uuid({
     message: "O formato do identificador não é UUID.",
   }),
-});
-
-const updateSchema = z.object({
-  nome: z.optional(
-    z.string()
-    .min(5, { message: "O nome da tarefa deve ter pelo menos 5 caracteres." })
-    .max(100, {
-      message: "O nome da tarefa não pode ter mais de 100 caracteres.",
-    })
-  ),
-  descricao: z.optional(
-    z.string()
-    .min(10, { message: "A descrição deve conter pelo menos 10 caracteres." })
-    .max(1000, { message: "A descrição não pode conter mais de 1000 cracteres." })
-  ),
-  status: z.optional(
-    z.enum(["pendente", "concluida"])
-  )
 });
 
 // Adicionar uma nova tarefa:
---
+++ src/validators/updateSchema.js
@@ -0,0 +1,19 @@
+const updateSchema = z.object({
+  nome: z.optional(
+    z.string()
+    .min(5, { message: "O nome da tarefa deve ter pelo menos 5 caracteres." })
+    .max(100, {
+      message: "O nome da tarefa não pode ter mais de 100 caracteres.",
+    })
+  ),
+  descricao: z.optional(
+    z.string()
+    .min(10, { message: "A descrição deve conter pelo menos 10 caracteres." })
+    .max(1000, { message: "A descrição não pode conter mais de 1000 cracteres." })
+  ),
+  status: z.optional(
+    z.enum(["pendente", "concluida"])
+  )
+});
+
+export default updateSchema

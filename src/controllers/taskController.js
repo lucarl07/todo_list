3fffe13d6418a4a7653367ade6a189515403e07b
lucarl07 <junitinho2007@gmail.com>
2024-09-02 10:05:59 -0300

    🔚 controller: Add validation for task updates

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -27,6 +27,24 @@ const taskIdSchema = z.object({
   }),
 });
 
+const updateSchema = z.object({
+  nome: z.optional(
+    z.string()
+    .min(5, { message: "O nome da tarefa deve ter pelo menos 5 caracteres." })
+    .max(100, {
+      message: "O nome da tarefa não pode ter mais de 100 caracteres.",
+    })
+  ),
+  descricao: z.optional(
+    z.string()
+    .min(10, { message: "A descrição deve conter pelo menos 10 caracteres." })
+    .max(1000, { message: "A descrição não pode conter mais de 1000 cracteres." })
+  ),
+  status: z.optional(
+    z.enum(["pendente", "concluida"])
+  )
+});
+
 // Adicionar uma nova tarefa:
 export const createNewTask = async (req, res) => {
   const bodyValidation = createSchema.safeParse(req.body);
@@ -124,6 +142,24 @@ export const getTaskByID = async (req, res) => {
 
 // Atualizar tarefa
 export const updateTask = async (req, res) => {
+  const paramsValidation = taskIdSchema.safeParse(req.params);
+
+  if (!paramsValidation.success) {
+    return res.status(400).json({
+      message: "Os dados recebidos da URL da requisição são inválidos.",
+      details: formatZodError(paramsValidation.error),
+    });
+  }
+
+  const bodyValidation = updateSchema.safeParse(req.body)
+
+  if (!bodyValidation.success) {
+    return res.status(400).json({
+      message: "Os dados recebidos da URL da requisição são inválidos.",
+      details: formatZodError(bodyValidation.error),
+    });
+  }
+
   const { id } = req.params;
   const { nome, descricao, status } = req.body;
 

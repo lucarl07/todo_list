68d8aa6bff5150c79637ef0cfe9a93f505095bc0
lucarl07 <junitinho2007@gmail.com>
2024-10-18 12:50:26 -0300

    ✨ feat: Add API interaction on the front-end

--- frontend/src/components/ToDoForm.jsx
+++ frontend/src/components/ToDoForm.jsx
@@ -1,19 +1,48 @@
 import { Form, Button } from 'react-bootstrap';
+import React from 'react';
+import axios from "axios";
 
 const ToDoForm = () => {
-  const handleSubmit = () => {}
+  const [tarefa, setTarefa] = React.useState('')
+  const [descricao, setDescricao] = React.useState('')
+  const [mensagem, setMensagem] = React.useState('')
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+  
+    try {
+      await axios.post('http://localhost:2608/tarefas/', {
+        nome: tarefa, descricao: descricao
+      })
+
+      setTarefa('')
+      setDescricao('')
+      setMensagem('Tarefa enviada com sucesso!')
+    } catch (error) {
+      setMensagem('Não foi possível salvar a sua tarefa')
+    }
+  }
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="novaTarefa.nomeTarefa">
         <Form.Label>Nome da tarefa</Form.Label>
-        <Form.Control type="text" placeholder="Ex.: Limpar quarto 012..." />
+        <Form.Control 
+          type="text" 
+          placeholder="Ex.: Limpar quarto 012..."
+          value={tarefa}
+          onChange={(e) => setTarefa(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="novaTarefa.descricaoTarefa">
         <Form.Label>Descrição da tarefa</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control 
+          as="textarea" 
+          rows={3}
+          value={descricao}
+          onChange={(e) => setDescricao(e.target.value)} />
       </Form.Group>
-      <Button type='submit'>Adicionar</Button>
+      <Button type='submit'>Adicionar tarefa</Button>
+      <p className='mt-3'>{mensagem}</p>
     </Form>
   );
 }
